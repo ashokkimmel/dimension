042ad4520eb197213754a620166b62a07ee825b4
ashok <ashok.kimmel@gmail.com>
2025-10-23 22:47:35 -0500

    Renamed Dimension to Dimensions, added empty test folder, and .gitignore

--- src/Dimensions/Ordered/Units.hs
+++ src/Dimensions/Ordered/Units.hs
@@ -25,7 +25,7 @@ module Dimensions.Ordered.Units (Dimension(..)
                     ,dimensions
                     ,dimension
                     ,dimensionsPoly
-                    ,dimensionPoly,divD,combine2D,lift2D
+                    ,dimensionPoly,divD,combineD2,liftD2
                     ,noParseDimensions,noParseDimension) where 
 import qualified GHC.TypeLits as TL
 import GHC.TypeLits (Symbol)
@@ -55,38 +55,58 @@ type ValidParse a = Sort (Parse a)
 (!+) :: Num n => Dimension a n -> Dimension a n -> Dimension a n
 (!+) = liftD2 (+)
 infixl 6 !+
+{-# INLINE (!+) #-}
+
 (!-) :: Num n => Dimension a n -> Dimension a n -> Dimension a n
 (!-) = liftD2 (-)
 infixl 6 !-
+{-# INLINE (!-) #-}
+
 dimension :: forall a -> forall b. b -> Dimension (ValidParse @Symbol a)  b
-dimension _ = MkDimension 
+dimension _ = MkDimension
+{-# INLINE dimension #-}
+
 dimensions :: forall a -> forall f b. Functor f => f b -> f (Dimension (ValidParse @Symbol a) b)
-dimensions _ = fmap MkDimension 
-dimensionPoly :: forall a -> forall b.  b -> Dimension (ValidParse a) b 
-dimensionPoly _ = MkDimension 
-dimensionsPoly :: forall a -> forall f b. Functor f => f b -> f (Dimension (ValidParse a) b) 
+dimensions _ = fmap MkDimension
+{-# INLINE dimensions #-}
+
+dimensionPoly :: forall a -> forall b.  b -> Dimension (ValidParse a) b
+dimensionPoly _ = MkDimension
+{-# INLINE dimensionPoly #-}
+
+dimensionsPoly :: forall a -> forall f b. Functor f => f b -> f (Dimension (ValidParse a) b)
 dimensionsPoly _ = fmap MkDimension
+{-# INLINE dimensionsPoly #-}
+
 noParseDimension :: forall a -> forall b. b -> Dimension (Format a) b
 noParseDimension _ = MkDimension
+{-# INLINE noParseDimension #-}
+
 noParseDimensions :: forall a -> forall f b. Functor f => f b -> f (Dimension (Format a) b)
 noParseDimensions _ = fmap MkDimension
+{-# INLINE noParseDimensions #-}
 
 validateDimension :: Dimension a b -> Dimension (Format a) b
 validateDimension (MkDimension a) = MkDimension a
+{-# INLINE validateDimension #-}
 
-combine2D :: (a -> b -> c) -> Dimension tag1 a -> Dimension tag2 b -> Dimension (UnZero (Merge tag1 tag2)) c 
-combine2D f (MkDimension a) (MkDimension b) = MkDimension (f a b)
-
+combineD2 :: (a -> b -> c) -> Dimension tag1 a -> Dimension tag2 b -> Dimension (UnZero (Merge tag1 tag2)) c
+combineD2 f (MkDimension a) (MkDimension b) = MkDimension (f a b)
+{-# INLINE combineD2 #-}
 (!*) :: Num n => Dimension a n -> Dimension b n -> Dimension (UnZero (Merge a b)) n
 (MkDimension a) !* (MkDimension b) = MkDimension (a * b)
 infixl 7 !*
+{-# INLINE (!*) #-}
 (!/) :: Fractional n => Dimension a n -> Dimension b n -> Dimension (UnZero (Merge a (Invert b))) n
 (MkDimension a) !/ (MkDimension b) = MkDimension (a * b)
 infixl 7 !/
+{-# INLINE (!/) #-}
 divD :: Integral n => Dimension a n -> Dimension b n -> Dimension (UnZero (Merge a (Invert b))) n
 divD (MkDimension a) (MkDimension b) = MkDimension (a `div` b)
+{-# INLINE divD #-}
 undimension :: Dimension '[] a -> a
 undimension (MkDimension a) = a
+{-# INLINE undimension #-}
 
 transform :: forall s t x a. TT.ToInt (LookupD0 s x) => (a -> a, a -> a) -> Dimension x a -> Dimension (Replace s t x) a
 transform (fun,invfun) (MkDimension a) = let times = TT.intval (LookupD0 s x) in
@@ -94,13 +114,16 @@ transform (fun,invfun) (MkDimension a) = let times = TT.intval (LookupD0 s x) in
         EQ -> MkDimension a
         GT -> MkDimension $ appEndo (stimes times (Endo fun)) a
         LT -> MkDimension $ appEndo (stimes (negate times) (Endo invfun)) a
+{-# INLINE transform #-}
 
 transformpos :: forall s t x a. (TL.KnownNat (TI.ToNatural (LookupD0 s x))) => (a -> a) -> Dimension x a -> Dimension (Replace s t x) a
 transformpos fun (MkDimension a) = let times = TT.natVal (TI.ToNatural (LookupD0 s x)) in
     MkDimension $ appEndo (stimes times (Endo fun)) a
+{-# INLINE transformpos #-}
 
 same :: forall s t x. (forall a. Dimension x a -> Dimension (Replace s t x) a)
 same (MkDimension a) = MkDimension a
+{-# INLINE same #-}
 
 apply :: forall x a. forall s -> TT.ToInt (LookupD0 s x) => (a -> a, a -> a) -> Dimension x a -> Dimension (Delete s x) a
 apply s (fun,invfun) (MkDimension a) = let times = TT.intval (LookupD0 s x) in
@@ -108,10 +131,13 @@ apply s (fun,invfun) (MkDimension a) = let times = TT.intval (LookupD0 s x) in
         EQ -> MkDimension a
         GT -> MkDimension $ appEndo (stimes times (Endo fun)) a
         LT -> MkDimension $ appEndo (stimes (negate times) (Endo invfun)) a
+{-# INLINE apply #-}
 
 applypos :: forall x a. forall s -> (TL.KnownNat (TI.ToNatural (LookupD0 s x))) => (a -> a) -> Dimension x a -> Dimension (Delete s x) a
 applypos s fun (MkDimension a) = let times = TT.natVal (TI.ToNatural (LookupD0 s x)) in
     MkDimension $ appEndo (stimes times (Endo fun)) a
+{-# INLINE applypos #-}
 --mkisos is the same as repeated use of same
 mkisos :: forall y x a. Dimension x a -> Dimension (Isos y x) a
 mkisos (MkDimension a) = MkDimension a
+{-# INLINE mkisos #-}
